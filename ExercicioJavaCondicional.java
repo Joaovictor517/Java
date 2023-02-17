c9f65b4d29a7ce3e320ff91050f1c83e6d2ea9c2
João Victor <80136828+Joaovictor517@users.noreply.github.com>
2023-02-17 18:40:18 -0300

    Add files via upload

---
+++ ExercicioJavaCondicional.java
@@ -0,0 +1,13 @@
+
+public class ExercicioJavaCondicional {
+
+	public static void main(String[] args) {
+			int nota1 = 7;
+			int nota2 = 2;
+			int nota3 = 6;
+			int nota4 = 7;
+		    String notaGeral = nota1, nota2, nota3, nota4;
+			System.out.println(notaGeral);
+		}
+
+}
