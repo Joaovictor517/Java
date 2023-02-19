f354e220f0c6f2364d9183bd0e827711d8c888fd
João Victor <80136828+Joaovictor517@users.noreply.github.com>
2023-02-19 09:56:11 -0300

    Update testaCondicional.java

--- testaCondicional.java
+++ testaCondicional.java
@@ -6,7 +6,7 @@ public class testaCondicional {
 		int idade = 16;
 		int quantidadedePessoas = 3;
 		if (idade >= 18) {
-			System.out.println("voce tem mais de 18 anos");
+			System.out.println("voce tem mais de 18 anos, fique a vontade");
 			System.out.println("seja bem vindo");
 		} else if (quantidadedePessoas >= 3) {
 			System.out.println("voce nao tem 18 mas pode entrar");
