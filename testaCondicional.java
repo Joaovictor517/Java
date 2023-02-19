bd00e6ee7c28b08dffed5cc07e60c4070873df7e
Victor <joaovictor.s517.j@gmail.com>
2023-02-19 09:49:40 -0300

    Alterando palavras

--- testaCondicional.java
+++ testaCondicional.java
@@ -9,12 +9,12 @@ public class testaCondicional {
         if (idade >= 18) {
 
             System.out.println("Você tem mais que 18 anos");
-            System.out.println("Seja bem vindo ");
+            System.out.println("Seja bem vindo, aqui neste lugar");
         } else {
             if (quantidadePessoas >= 2) {
                 System.out.println("voce nao tem 18, mas pode entrar, pois está acompanhado");
             } else {
-                System.out.println("infelizmente voce nao pode entrar");
+                System.out.println("infelizmente voce nao pode entrar, que pena.");
             }
         }
     }
