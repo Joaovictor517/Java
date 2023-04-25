e66b9f44d39305954941c216deb5f7ccc200da45
João <joaovictor.s517.j@gmail.com>
2023-04-25 16:13:05 -0300

    Resolvendo Bugs

--- Algoritimo01.java
+++ Algoritimo01.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
+
 public class Algoritimo01 {
-    
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -10,36 +11,33 @@ public class Algoritimo01 {
         int diaNascimento;
         int mesNascimento;
         int anoNascimento;
-
+    
         
-
+    
         System.out.println("Escreva o dia do seu nascimento!!");
         diaNascimento = sc.nextInt();
-
+    
         System.out.println("Escreva o mês do seu nascimento!!");
         mesNascimento = sc.nextInt();
         
         System.out.println("Escreva o ano do seu nascimento!!");
         anoNascimento = sc.nextInt();
-
+    
         // Transformando ano em dia
         int idadeAno = anoAtual - anoNascimento;
         int idadeMes = idadeAno * 12;
         int idadeDia = (idadeMes * qtdadeDiaMes);
         int idadeDemasMeses = 0;
-
+    
         // Calculo dos dias do mês
         if (mesNascimento > mesAtual){
             idadeDemasMeses = mesAtual * qtdadeDiaMes;
         }else if (mesNascimento < mesAtual){
             idadeDemasMeses = (mesAtual - mesNascimento) * qtdadeDiaMes;
         }
-
+    
         int total = idadeDia + idadeDemasMeses + diaNascimento;
-
+    
         System.out.println("Idade em dias : " + total);
-
-        
-    }
-    
+    }
 }
--- Algoritmo02.java
+++ Algoritmo02.java
@@ -1,17 +1,15 @@
 import java.util.Scanner;
 
 public class Algoritmo02 {
-
 	public static void main(String[] args) {
-
-			Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 			String NomeAluno;
 		    double nota1, nota2, nota3, nota4;
-
+			
 			System.out.println("Digite seu nome.");
-			nota1 = sc.next();
-
+			NomeAluno = sc.next();
+			
 			System.out.println("Digite sua nota da primeira avalição.");
 			nota1 = sc.nextDouble();
 
@@ -23,20 +21,14 @@ public class Algoritmo02 {
 
 			System.out.println("Digite sua nota da quarta avalição."); 
 			nota4 = sc.nextDouble();
-
-		    //calculando nota
-			//double media1 = nota1 *2;
-			//double media2 = nota2 *3;
-			//double media3 = nota3 *2;
-			//double media4 = nota4 *3;
-
-		 	double MediaGeral = (nota1 *2) + (nota2*3) + (nota3*2) + (nota4*3); // (2+3+2+3);
+			
+			double MediaGeral = (nota1*2) + (nota2*3) + (nota3*2) + (nota4*3);
 
 			//verificando média
 			if (MediaGeral >= 50) {
-				System.out.println("Parabéns" + NomeAluno + " , você está aprovado!! e sua média foi: " + MediaGeral);
+				System.out.println("Parabéns " + NomeAluno + ", você está aprovado!! e sua média foi de: " + MediaGeral);
 			} else {
-				System.out.println("Que pena" + NomeAluno + ", você está reprovado!! sua média foi: " + MediaGeral);
+				System.out.println("Que pena " + NomeAluno + ", você está reprovado!! sua média foi de: " + MediaGeral);
 			}
 		}
 }
