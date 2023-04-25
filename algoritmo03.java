9d5a185cd9047359891a2a27b77350ec2376a4b5
João <joaovictor.s517.j@gmail.com>
2023-04-25 11:37:53 -0300

    Alterando nomes de arquivos

---
+++ algoritmo03.java
@@ -0,0 +1,15 @@
+public class algoritmo03 {
+    public static void main(String[] args) {
+        int contador = 0;
+        int total = 6;
+
+        while(contador <=100) {
+        }
+    }
+
+}
+
+
+    //total = total + contador;
+        //contador++;
+//System.out.print(total);
