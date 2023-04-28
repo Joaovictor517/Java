c2385230e942dd192c7a0d3573061f5942a5e4cc
João <joaovictor.s517.j@gmail.com>
2023-04-27 23:05:40 -0300

    Praticando a função switch

---
+++ PraticaComSwitch.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class PraticaComSwitch {
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        int idade;
+
+            System.out.println("Digite a sua idade:");
+            idade = sc.nextInt();
+
+                switch(idade) {
+                    case 5:
+                    case 6:
+                    case 7:
+                        System.out.println("Você entra na categoria Infantil A, Parabéns!!");
+                        break;
+                    case 8:
+                    case 9:
+                    case 10:
+                        System.out.println("Você entra na categoria Infantil B, Parabéns!!");
+                        break;
+                    case 11:
+                    case 12:
+                    case 13:
+                        System.out.println("Você entra na categoria Juvenil A, Parabéns!!");
+                        break;
+                    case 14:
+                    case 15:
+                    case 16:
+                    case 17:
+                        System.out.println("Você entra na categoria Juvenil B, Parabéns!!");
+                        break;
+                    case 18:
+                    default:
+                        System.out.println("Você entra na categoria Adulto, Parabéns!!");
+                        break;
+            }
+        }
+    }
--- algoritmo03.java
+++ algoritmo03.java
@@ -1,10 +1,6 @@
 public class algoritmo03 {
     public static void main(String[] args) {
-        int contador = 0;
-        int total = 6;
-
-        while(contador <=100) {
-        }
+        for ()
     }
 
 }
