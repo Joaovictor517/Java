834f5319d2a79cc198789a3a390e63237e22c0e1
João <joaovictor.s517.j@gmail.com>
2023-04-28 10:10:14 -0300

    Fazendo Alterações 2.0

--- Algoritmo04.java
+++ Algoritmo04.java
@@ -1,39 +1,31 @@
+//**Elabore um algoritmo que dada a idade de um nadador classifica-o em uma das seguintes categorias:**
+//
+//- infantil A = 5 - 7 anos
+//- infantil B = 8-10 anos
+//- juvenil A = 11-13 anos
+//- juvenil B = 14-17 anos
+//- adulto = maiores de 18 anos 
+
 import java.util.Scanner;
+    public class Algoritmo04 {
+        public static void main(String []args) {
+        Scanner sc= new Scanner(System.in);
 
-public class Algoritmo04 {
-    public static void main(String []args) {
-        Scanner sc = new Scanner(System.in);
         int idade;
+        
+        System.out.println("Qual a sua idade?");
+        idade = sc.nextInt();
 
-            System.out.println("Digite a sua idade:");
-            idade = sc.nextInt();
-
-                switch(idade) {
-                    case 5:
-                    case 6:
-                    case 7:
-                        System.out.println("Você entra na categoria Infantil A, Parabéns!!");
-                        break;
-                    case 8:
-                    case 9:
-                    case 10:
-                        System.out.println("Você entra na categoria Infantil B, Parabéns!!");
-                        break;
-                    case 11:
-                    case 12:
-                    case 13:
-                        System.out.println("Você entra na categoria Juvenil A, Parabéns!!");
-                        break;
-                    case 14:
-                    case 15:
-                    case 16:
-                    case 17:
-                        System.out.println("Você entra na categoria Juvenil B, Parabéns!!");
-                        break;
-                    case 18:
-                    default:
-                        System.out.println("Você entra na categoria Adulto, Parabéns!!");
-                        break;
-            }
-        }
+        if (idade >=5 && idade <=7) {
+            System.out.println("Você entra na categoria Infantil A!!");
+        } else if (idade >=8 && idade <=10) {
+            System.out.println("Você entra na categoria Infantil B");
+        } else if (idade >= 11 && idade <=13) {
+            System.out.println("Você entra na categoria Juvenil A!!");
+        } else if (idade >= 14 && idade <=17) {
+            System.out.println("Você entra na categoria Juvenil B!!");
+        } else if (idade >=18) {
+            System.out.println("Você entra na categoria adulto");
+        } 
     }
+ }
--- AlgoritmoSA04.java
+++ AlgoritmoSA04.java
@@ -6,28 +6,42 @@
 //- juvenil B = 14-17 anos
 //- adulto = maiores de 18 anos
 
+ import java.util.Scanner;
 
-import java.util.Scanner;
 public class AlgoritmoSA04 {
-    public static void mian(String []args) {
-        Scanner sc= new Scanner(System.in);
-
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
         int idade;
-        
-        System.out.println("Qual a sua idade?");
-        idade = sc.nextInt();
 
-        if (idade >=5 || idade <7) {
-            System.out.println("Você entra na categoria Infantil A!!");
-        } else if (idade >=8 || idade <10) {
-            System.out.println("Você entra na categoria Infantil B");
-        } else if (idade >= 11 || idade <13) {
-            System.out.println("Você entra na categoria Juvenil A!!");
-        } else if (idade >= 14 || idade <17) {
-            System.out.println("Você entra na categoria Juvenil B!!");
-        } else if (idade >=18) {
-            System.out.println("Você entra na categoria adulto");
-        } 
+            System.out.println("Digite a sua idade:");
+            idade = sc.nextInt();
+
+                switch(idade) {
+                    case 5:
+                    case 6:
+                    case 7:
+                        System.out.println("Você entra na categoria Infantil A, Parabéns!!");
+                        break;
+                    case 8:
+                    case 9:
+                    case 10:
+                        System.out.println("Você entra na categoria Infantil B, Parabéns!!");
+                        break;
+                    case 11:
+                    case 12:
+                    case 13:
+                        System.out.println("Você entra na categoria Juvenil A, Parabéns!!");
+                        break;
+                    case 14:
+                    case 15:
+                    case 16:
+                    case 17:
+                        System.out.println("Você entra na categoria Juvenil B, Parabéns!!");
+                        break;
+                    case 18:
+                    default:
+                        System.out.println("Você entra na categoria Adulto, Parabéns!!");
+                        break;
+            }
+        }
     }
- }
-
